e7142ec71e0c36876e1d3373bd4dc3c3c252de9c
gaozw <private_gzw1314@163.com>
2023-03-16 14:52:19 +0800

    commit 1

--- src/main/java/com/gzw/kd/KdApplication.java
+++ src/main/java/com/gzw/kd/KdApplication.java
@@ -8,11 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 /**
  * @author 高志伟
  */
-
+@EnableDiscoveryClient
 @ServletComponentScan
 @EnableScheduling
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
