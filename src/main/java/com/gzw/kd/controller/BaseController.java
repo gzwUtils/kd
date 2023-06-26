1bddf94b6804a1951517e751115aa49a196b257f
gaozw <private_gzw1314@163.com>
2023-06-26 22:25:53 +0800

    0626 002

--- src/main/java/com/gzw/kd/controller/BaseController.java
+++ src/main/java/com/gzw/kd/controller/BaseController.java
@@ -3,20 +3,20 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.gzw.kd.common.R;
 import com.gzw.kd.common.annotation.OperatorLog;
-import com.gzw.kd.common.utils.EventBusUtils;
-import com.gzw.kd.common.utils.MyLinkedBlockQueue;
+import com.gzw.kd.common.entity.Operator;
+import com.gzw.kd.common.generators.SnowIdGenerator;
+import com.gzw.kd.common.utils.*;
 import com.gzw.kd.common.entity.MyQueue;
-import com.gzw.kd.common.utils.RedisLimitFlow;
 import com.gzw.kd.learn.model.callback.Client;
 import com.gzw.kd.learn.model.callback.Server;
+import com.gzw.kd.learn.model.filter.DemoGzwFilterChain;
+import com.gzw.kd.learn.model.filter.DemoGzwFilterStage;
 import com.gzw.kd.learn.model.model.GzwThreadDemo;
 import com.gzw.kd.vo.input.ValidTest;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import java.util.ArrayList;
+import com.itextpdf.xmp.impl.Base64;
+import io.swagger.annotations.*;
 import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -47,12 +47,12 @@ public class BaseController {
     @Resource
     private EventBusUtils  eventBusUtils;
 
+    @Resource
+    private DemoGzwFilterChain demoGzwFilterChain;
 
-    private static List<String> put_list = new ArrayList<>();
-
-    private static List<String> take_list = new ArrayList<>();
-
-
+    @ApiOperation(value = "入队 阻塞")
+    @OperatorLog(value = "入队 阻塞 ",description = "入队 阻塞")
+    @ApiImplicitParams({@ApiImplicitParam(name = "Accept-Language", value = "语言", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "other-token", value = "token校验", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "sign", value = "签名", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "timestamp", value = "时间戳", paramType = "header", dataTypeClass = String.class, required = false)})
     @RequestMapping(value = "/put", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
     public R put(@RequestBody MyQueue myQueue) {
         if (myQueue.getSize() != null) {
@@ -67,14 +67,15 @@ public class BaseController {
                     log.error("test put method Interrupt", e);
                 }
                 linkedBlockQueue.put(myQueue.getData().get(i));
-                log.info("生产者生产了：{} {}", i, JSONObject.toJSONString(myQueue.getData().get(i)));
-                put_list.add("生产者:"+myQueue.getData().get(i));
+                log.info("入队：{} {}", i, JSONObject.toJSONString(myQueue.getData().get(i)));
             }
         }, "put").start();
-        return R.ok().data("pushs", String.join(",",put_list));
+        return R.ok();
     }
 
-
+    @ApiOperation(value = "出队 阻塞")
+    @OperatorLog(value = "出队 阻塞 ",description = "出队 阻塞")
+    @ApiImplicitParams({@ApiImplicitParam(name = "Accept-Language", value = "语言", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "other-token", value = "token校验", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "sign", value = "签名", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "timestamp", value = "时间戳", paramType = "header", dataTypeClass = String.class, required = false)})
     @RequestMapping(value = "/take", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
     public R take(@RequestBody MyQueue myQueue) {
         if (myQueue.getSize() != null) {
@@ -89,21 +90,13 @@ public class BaseController {
                     log.error("test take method Interrupt", e);
                 }
                 Object take = linkedBlockQueue.take();
-                log.info("消费者消费了：{} {}", i,JSONObject.toJSONString(take));
-                take_list.add("消费者"+JSONObject.toJSONString(take));
+                log.info("出队：{} {}", i,JSONObject.toJSONString(take));
             }
         }, "take").start();
-        return R.ok().data("takes",String.join(",",take_list));
-    }
-
-
-    @RequestMapping(value = "/clear",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
-    public  R clear (){
-        put_list.clear();
-        take_list.clear();
         return R.ok();
     }
 
+    @ApiOperation(value = "redis 限流")
     @OperatorLog(value = "redis limit flow ",description = "redis 限流")
     @RequestMapping(value = "/limitFlow",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
     public  R limitFlow (){
@@ -111,12 +104,16 @@ public class BaseController {
     }
 
 
+    @ApiOperation(value = "开启线程")
+    @OperatorLog(value = "开启线程",description = "学习")
     @RequestMapping(value = "/gzwThreadDemo",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
     public  R start (){
         gzwThreadDemo.start();
         return R.ok();
     }
 
+    @ApiOperation(value = "关闭线程")
+    @OperatorLog(value = "关闭线程",description = "学习")
     @RequestMapping(value = "/shutdown", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
     public R shutdown() {
         gzwThreadDemo.shutdown();
@@ -124,9 +121,9 @@ public class BaseController {
     }
 
 
-    @ApiImplicitParams({@ApiImplicitParam(name = "Accept-Language", value = "语言", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "other-token", value = "token校验", paramType = "header", dataTypeClass = String.class, required = true),@ApiImplicitParam(name = "sign", value = "签名", paramType = "header", dataTypeClass = String.class, required = true),@ApiImplicitParam(name = "timestamp", value = "时间戳", paramType = "header", dataTypeClass = String.class, required = true)})
+    @ApiImplicitParams({@ApiImplicitParam(name = "Accept-Language", value = "语言", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "other-token", value = "token校验", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "sign", value = "签名", paramType = "header", dataTypeClass = String.class, required = false),@ApiImplicitParam(name = "timestamp", value = "时间戳", paramType = "header", dataTypeClass = String.class, required = false)})
     @ApiOperation(value = "参数校验")
-    @OperatorLog(value = "责任链模式",description = "学习")
+    @OperatorLog(value = "参数校验",description = "学习")
     @RequestMapping(value = "/valid", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
     public R valid(@RequestBody @Validated ValidTest validTest){
         log.info("valid ........{}", JSON.toJSONString(validTest));
@@ -137,7 +134,7 @@ public class BaseController {
     @ApiOperation(value = "消息事件推送")
     @OperatorLog(value = "观察者模式",description = "学习")
     @RequestMapping(value = "/push", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
-    public R push(@RequestParam("msg") String msg) {
+    public R push(@RequestParam("msg") @ApiParam("消息") String msg) {
         eventBusUtils.asyncEventPost(msg);
         return R.ok();
     }
@@ -145,9 +142,47 @@ public class BaseController {
     @ApiOperation(value = "回调")
     @OperatorLog(value = "回调",description = "学习")
     @RequestMapping(value = "/call", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
-    public R call(@RequestParam("msg") String msg) {
+    public R call(@RequestParam("msg") @ApiParam("消息") String msg) {
         Client client = new Client(new Server());
         client.sendMsg(msg);
         return R.ok();
+    }
+
+
+    @ApiOperation(value = "base64解码")
+    @RequestMapping(value = "/base64",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
+    public R base64Decode(@RequestParam("param") @ApiParam("参数") String param){
+        String decode = Base64.decode(param);
+        return R.ok().data("code",decode);
+    }
+
+    @ApiOperation(value = "雪花ID生成")
+    @OperatorLog(value = "雪花ID生成",description = "学习")
+    @RequestMapping(value = "/snoIdGenerator", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public R snoIdGenerator(){
+        Long generatorId = SnowFlakeIdUtils.generatorId();
+        return R.ok().data("id",generatorId);
+    }
+
+    @ApiOperation(value = "ID生成")
+    @OperatorLog(value = "ID生成",description = "学习")
+    @RequestMapping(value = "/idGenerator", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public R idGenerator(@RequestParam(value = "bizTypePrefix") @ApiParam("前缀") String bizTypePrefix,@RequestParam(value = "dateFormat",defaultValue = "yyMMdd",required = false) @ApiParam("时间格式") String dateFormat){
+        SnowIdGenerator idGenerator = new SnowIdGenerator(bizTypePrefix, dateFormat);
+        return R.ok().data("id",idGenerator.generate());
+    }
+
+
+    @ApiOperation(value = "责任链模式")
+    @OperatorLog(value = "责任链模式",description = "学习")
+    @RequestMapping(value = "/filterChain", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public R filterChain(){
+        Operator operator = (Operator) ContextUtil.getHttpRequest().getSession().getAttribute(LOGIN_USER_SESSION_KEY);
+        DemoGzwFilterStage demoGzwFilterStage = new DemoGzwFilterStage();
+        demoGzwFilterStage.setOperator(operator);
+        DemoGzwFilterStage stage = demoGzwFilterChain.doFilter(demoGzwFilterStage);
+        List<Map<String, Object>> result = stage.getStageHandlerResult();
+        log.info("chain end {}", JSON.toJSONString(result));
+        return R.ok().data("result",result);
     }
 }
