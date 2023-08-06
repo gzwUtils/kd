86de2ac46b3efb29e6969474b802edaa6b6d8939
gaozw <private_gzw1314@163.com>
2023-08-07 00:04:10 +0800

    001

--- src/main/java/com/gzw/kd/common/utils/RedisLimitFlow.java
+++ src/main/java/com/gzw/kd/common/utils/RedisLimitFlow.java
@@ -27,18 +27,18 @@ public class RedisLimitFlow {
 
 
 
-    public static R limitFlow(long intervalTime,int  limitCount){
+    public static R limitFlow(String key,long intervalTime,int  limitCount){
         long time = System.currentTimeMillis();
-        Boolean limit = redisTemplate.hasKey("limit");
+        Boolean limit = redisTemplate.hasKey(key);
         if(Boolean.TRUE.equals(limit)){
-            int count = redisTemplate.opsForZSet().rangeByScore("limit", time - intervalTime, time).size();
+            int count = redisTemplate.opsForZSet().rangeByScore(key, time - intervalTime, time).size();
             log.info("limit count {}",count);
             if(count >= limitCount){
                 int minute = (int) (intervalTime/1000/60);
-                return R.error().message(minute+"分钟最多只能访问"+limitCount+"次");
+                return R.error().message("请求太频繁 请稍后重试..........");
             }
         }
-        redisTemplate.opsForZSet().add("limit", UUID.randomUUID().toString(),time);
+        redisTemplate.opsForZSet().add(key, UUID.randomUUID().toString(),time);
         return R.ok();
     }
 }
--- src/main/java/com/gzw/kd/config/CacheConfig.java
+++ src/main/java/com/gzw/kd/config/CacheConfig.java
@@ -2,7 +2,6 @@ package com.gzw.kd.config;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,7 +18,7 @@ public class CacheConfig {
     public Cache<String,Object> caffeineCache(){
         return Caffeine.newBuilder()
                 // 设置最后一次写入或访问后经过固定时间过期
-                .expireAfterWrite(60 * 20, TimeUnit.SECONDS)
+                //.expireAfterWrite(60 * 20, TimeUnit.SECONDS)
                 // 初始的缓存空间大小
                 .initialCapacity(100)
                 // 缓存的最大条数
