d4c295d8921a7120605a4f1d184cdd017fdd8cc0
gaozw <private_gzw1314@163.com>
2024-03-26 16:48:47 +0800

    0326001 commit

--- src/main/java/com/gzw/kd/cache/GzwCache.java
+++ src/main/java/com/gzw/kd/cache/GzwCache.java
@@ -1,6 +1,7 @@
 package com.gzw.kd.cache;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -9,19 +10,19 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * @description：
  * @since：2023/8/10 10:45
  */
-@SuppressWarnings("all")
+@SuppressWarnings({"unused","unchecked","rawtypes"})
 public class GzwCache extends LinkedHashMap {
 
-    private ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-    private Lock readLock = readWriteLock.readLock();
-    private Lock writeLock = readWriteLock.writeLock();
+    private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Lock readLock = readWriteLock.readLock();
+    private final Lock writeLock = readWriteLock.writeLock();
     /**
      * 缓存大小限制
      */
-    private int maxSize;
+    private final int maxSize;
 
     public GzwCache (int maxSize){
-        super(maxSize + 1, 1.0f, true);
+        super(maxSize, 0.75f, true);
         this.maxSize = maxSize;
     }
 
@@ -43,5 +44,22 @@ public class GzwCache extends LinkedHashMap {
         } finally {
             writeLock.unlock();
         }
+    }
+
+    /**
+     * 当缓存容量达到上限时移除最久未使用的节点
+     * @param eldest The least recently inserted entry in the map, or if
+     *           this is an access-ordered map, the least recently accessed
+     *           entry.  This is the entry that will be removed it this
+     *           method returns <tt>true</tt>.  If the map was empty prior
+     *           to the <tt>put</tt> or <tt>putAll</tt> invocation resulting
+     *           in this invocation, this will be the entry that was just
+     *           inserted; in other words, if the map contains a single
+     *           entry, the eldest entry is also the newest.
+     * @return true
+     */
+    @Override
+    protected boolean removeEldestEntry(Map.Entry eldest) {
+        return size() > maxSize;
     }
 }
