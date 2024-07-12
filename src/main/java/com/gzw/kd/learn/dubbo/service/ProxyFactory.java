5b8c47cc37d30a01358caeef8fc9abce58daa51c
gaozw <private_gzw1314@163.com>
2024-07-12 23:23:32 +0800

    001

--- src/main/java/com/gzw/kd/learn/dubbo/service/ProxyFactory.java
+++ src/main/java/com/gzw/kd/learn/dubbo/service/ProxyFactory.java
@@ -28,6 +28,7 @@ public class ProxyFactory implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         log.warn("----------"+method);
+        method.invoke(proxy,args);
         return null;
     }
 }
